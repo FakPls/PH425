600d52217a2b394e9723f71f37095f07ac858d73
FakPls <gassmasterglen@gmail.com>
2024-01-30 23:33:24 -0700

    peak finding

--- Zeeman Effect/detect intensity.py
+++ Zeeman Effect/detect intensity.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.signal import argrelextrema, find_peaks
 
 im = Image.open('Zeeman Effect\TEST.png')
 im = im.convert('LA')
@@ -13,5 +14,10 @@ intensity = np.zeros(x)
 for i in range(x):
     intensity[i] = px[i,y/2][0]
 
+max = argrelextrema(intensity, np.greater)
+max_2, _ = find_peaks(intensity, height = np.max(intensity)/2)
+
 plt.plot(xPixels, intensity, linewidth = 1)
+# plt.vlines(max, 0, np.max(intensity), color = 'red')
+plt.vlines(max_2, 0, np.max(intensity), color = 'green')
 plt.show()
