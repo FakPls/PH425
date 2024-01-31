7dce4093802792977e2ae6c7c51763e0cf61e891
FakPls <gassmasterglen@gmail.com>
2024-01-30 23:02:21 -0700

    fixed plot

--- Zeeman Effect/detect intensity.py
+++ Zeeman Effect/detect intensity.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 im = Image.open('Zeeman Effect\TEST.png')
 im = im.convert('LA')
 x, y = im.size
@@ -12,7 +11,7 @@ xPixels = np.arange(0, x)
 px = im.load()
 intensity = np.zeros(x)
 for i in range(x):
-    intensity[i] = px[i,0][0]
+    intensity[i] = px[i,y/2][0]
 
-plt.plot(xPixels, intensity)
+plt.plot(xPixels, intensity, linewidth = 1)
 plt.show()
