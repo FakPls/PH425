05cb86932f16cc692ea502bc6c062eeed4a6fe16
FakPls <gassmasterglen@gmail.com>
2024-02-06 18:50:10 -0700

    gifs

--- Zeeman Effect/IntensityAni.py
+++ Zeeman Effect/IntensityAni.py
@@ -11,16 +11,20 @@ ti  = time.time()
 
 im = Image.open('Zeeman Effect\Lab Images\Yellow 4.png')
 im = im.convert('LA')
-x, y = im.size
-# im.show()
+
+cropped_im = im.crop((704, 143, 5656, 3727))
+cropped_im = cropped_im.rotate(-90)
+x, y = cropped_im.size
+# cropped_im.show()
 xPixels = np.arange(0, x)
 
 #Record Intensity/intensity of pixels along x
 
 plots = []
 
-px = im.load()
-slices_to_pixels = int(y/200)
+px = cropped_im.load()
+slices = 200
+slices_to_pixels = int(y/slices)
 
 for j in range(0, y, slices_to_pixels):
     intensity = np.zeros(x)
@@ -34,10 +38,10 @@ def animate(i):
 
 fig, ax = plt.subplots(1, 1, figsize = (8,8))
 ax.set_xlim(0, x)
-ax.set_ylim(0, 300)
+ax.set_ylim(0, 255)
 ln1, = plt.plot([], [], lw=1)
 ani = animation.FuncAnimation(fig, animate, frames=len(plots), interval=10)
 ani.save('pen.gif',writer='pillow',fps=24)
 
 tf = time.time()
-print('Computation Time: ', tf - ti)
+print('Computation Time:', int(tf - ti), 's')
