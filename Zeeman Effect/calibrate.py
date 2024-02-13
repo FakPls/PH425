f294cc075e8d3f9ce273ecf482707829e67a86a2
FakPls <gassmasterglen@gmail.com>
2024-02-13 16:29:04 -0700

    fixed calibration

--- Zeeman Effect/calibrate.py
+++ Zeeman Effect/calibrate.py
@@ -3,14 +3,14 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
 voltage = np.arange(0, 3, 0.1)
-field = np.array([34.1, 62.2, 110.3, 163.2, 220, 278, 335, 390, 446, 499, 550, 600, 650, 697, 745, 791, 837, 879, 919, 957, 992, 1025, 1054, 1082, 1106, 1129, 1151, 1170, 1187, 1203]) / 1000 #T
+field = np.array([29.9, 55.1, 97.8, 114.8, 195.3, 246, 299, 347, 397, 444, 489, 534, 577, 619, 661, 701, 739, 775, 809, 842, 870, 898, 920, 943, 961, 979, 993, 1009, 1023, 1035]) / 1000 #T
 
 def f_tri(x, a, b, c, d):
     return a*x**3 + b*x**2 + c*x+ d
 
-popt, pcov = curve_fit(f_tri, voltage, field, p0 = [0, 0, .5, 0.02])
+popt, pcov = curve_fit(f_tri, field, voltage,p0 = [0, 0, .5, 0.02])
 a_opt, b_opt, c_opt, d_opt = popt
-xModel = np.linspace(np.min(voltage), np.max(voltage), 1000)
+xModel = np.linspace(np.min(field), np.max(field), 1000)
 yModel = f_tri(xModel, a_opt, b_opt, c_opt, d_opt)
 
 co = np.polyfit(voltage, field, 3)
@@ -23,11 +23,11 @@ fig, axes= plt.subplots(1, 2, figsize = (8, 6))
 ax = axes[0]
 bx = axes[1]
 
-ax.scatter(voltage, field, s = 15, color = 'blue', label = 'Data')
+ax.scatter(field, voltage, s = 15, color = 'blue', label = 'Data')
 ax.plot(xModel, yModel, color = 'red', label = 'Fit')
 ax.set_title('Voltage vs Field')
-ax.set_xlabel('Voltage [V]')
-ax.set_ylabel('B Field [T]')
+ax.set_ylabel('Voltage [V]')
+ax.set_xlabel('B Field [T]')
 ax.legend()
 ax.grid()
 
