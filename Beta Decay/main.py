cc7b13d0639860ca0e90e0c72cf246cb9c97821e
FakPls <gassmasterglen@gmail.com>
2024-04-03 20:37:15 -0600

    Update main.py

--- Beta Decay/main.py
+++ Beta Decay/main.py
@@ -1,14 +1,20 @@
 import numpy as np
 from scipy.optimize import curve_fit
 from matplotlib import pyplot as plt
+from scipy.signal import find_peaks
 
 def f_lin_inverse(x, a, b):
     return (x - b)/a
+
+def f_gauss(x, A, mu, sig):
+    return A*np.exp(-(x-mu)**2/sig**2)
 
 files = [
     'Beta Decay\Data\Manual Run 1.csv'
     # 'Beta Decay\Data\Manual Run 1.txt'
 ]
+
+incriment = 0.1
 
 A, B = np.genfromtxt('Beta Decay\Data\Calibration_Parameters.txt', delimiter = ',')
 
@@ -20,11 +26,26 @@ for file in files:
     count = np.append(count, c)
     run = np.append(run, r)
 
-voltage = run * 0.1
+voltage = run * incriment
 field = f_lin_inverse(voltage, A, B)
 
 
-plt.xlabel('Field [mT]')
-plt.ylabel('Counts [N]')
-plt.plot(field, count)
+
+max, _ = find_peaks(count, height = np.max(count)/2)
+
+print(max)
+
+
+fig, axes = plt.subplots(2, 1, figsize = (6, 6), constrained_layout = True)
+
+ax = axes[0]
+
+ax.set_title('Field vs Counts')
+ax.set_xlabel('Field [mT]')
+ax.set_ylabel('Counts [#]')
+ax.grid()
+ax.plot(field, count, label = 'Counts')
+ax.legend()
+
+
 plt.show()
