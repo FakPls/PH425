5a55dacc576082c4def1c42427fa8fcf8d8409e4
FakPls <gassmasterglen@gmail.com>
2024-04-17 19:39:19 -0600

    trying deconvolution

--- Beta Decay/main.py
+++ Beta Decay/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import curve_fit
 from scipy.signal import find_peaks
+from scipy.signal import deconvolve, convolve
 from matplotlib import pyplot as plt
 
 
@@ -87,6 +88,10 @@ print('Gaussian Parameters: [A: %3f, Mu: %3f, Sigma: %3f]' % (a_opt, mu_opt, sig
 print('Errors on Gaussian Parameters: [A: %3f, Mu: %3f, Sigma: %3f]' % totuple(np.sqrt(np.diag(pcov))))
 print('Average Background: %3f Counts' % avg_background)
 
+deconvolved_signal, remainder = deconvolve(count, y_gauss)
+
+print(deconvolved_signal)
+
 ###################################################################################################
 #PLOTTING
 
@@ -101,6 +106,7 @@ ax.set_xlabel('Field [mT]')
 ax.set_ylabel('Counts [#]')
 ax.grid()
 ax.errorbar(field, count, yerr = count_err, capsize = 3, label = 'Raw Data')
+ax.plot(field, deconvolved_signal, label = 'Deconvolution', color = 'black')
 ax.plot(x_gauss, y_gauss, label = 'Fit', color = 'red')
 ax.scatter(field[max], count[max], marker = 'x', label = 'Maxima', color = 'green')
 ax.legend()
