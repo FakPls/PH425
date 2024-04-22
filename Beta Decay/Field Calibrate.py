092addc78be409aebe8965d689089bb2b2fc7799
FakPls <gassmasterglen@gmail.com>
2024-04-21 23:11:01 -0600

    data

--- Beta Decay/Field Calibrate.py
+++ Beta Decay/Field Calibrate.py
@@ -12,6 +12,7 @@ field = np.array([7.93, 7.93, 7.93, 7.93, 7.95, 7.92, 7.93, 9.24, 14.44,
                   20.835, 26.995, 34.0, 41.3, 48.7, 56.0, 63.2, 70.3, 77.2, 
                   83.9, 90.3, 96.8, 103.0, 109.4, 115.6, 121.8, 122.8])
 
+voltage_err = 0.1
 
 voltage = voltage[7:-1]
 field = field[7:-1]
@@ -33,6 +34,7 @@ plt.ylabel('Voltage [V]')
 plt.xlabel('Field [mT]')
 plt.plot(xmodel, ymodel, color = 'red', label = 'Fit')
 plt.scatter(field, voltage, color = 'black', s = 15, label = 'Data')
+plt.errorbar(field, voltage, yerr = voltage_err, color = 'black', ls = 'none')
 plt.legend()
 plt.grid()
 plt.show()
