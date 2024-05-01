8a114d4cd2412d9e4339a1d0c29baafd66a4dc57
FakPls <gassmasterglen@gmail.com>
2024-04-30 20:08:18 -0600

    data

--- Cosmic Rays/main.py
+++ Cosmic Rays/main.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 
 
 
-def f_cos_2(x, A, n, phi, B):
-    return A * np.cos(np.deg2rad(n * x + phi))**2 + B
+def f_cos_2(x, A, n, B):
+    return A * np.cos(np.deg2rad(n * x))**2 + B
 
 def reject_outliers(data, m = 1):
     d = np.abs(data - np.median(data))
@@ -37,30 +37,30 @@ angle_err = np.zeros(len(angle)) + 10
 counts = []
 
 for item in raw_csv:
-    counts.append(np.sum(item))
+    print(np.sort(item))
+    temp = np.sort(item)[3:-2]
+    print(temp)
+    counts.append(np.sum(temp))
     
 count_err = np.sqrt(counts)
 
-popt, pcov = curve_fit(f_cos_2, angle, counts, sigma = count_err, p0 = [0, 0, 0, 0])
-A_opt, n_opt, phi_opt, B_opt = popt
-A_opt_err, n_opt_err, phi_opt_err, B_opt_err = totuple(np.sqrt(np.diag(pcov)))
+popt, pcov = curve_fit(f_cos_2, angle, counts, sigma = count_err, p0 = [0, 0, 0])
+A_opt, n_opt, B_opt = popt
+A_opt_err, n_opt_err, B_opt_err = totuple(np.sqrt(np.diag(pcov)))
 
 x_model = np.linspace(np.min(angle), np.max(angle), 1000)
-y_model = f_cos_2(x_model, A_opt, n_opt, phi_opt, B_opt)
+y_model = f_cos_2(x_model, A_opt, n_opt, B_opt)
 
-print('COS Optimal Parameters:          [A: %.4f, n: %.4f, phi: %.4f, B: %.4f]' % (A_opt, 
-                                                                                 n_opt, 
-                                                                                 phi_opt, 
+print('COS Optimal Parameters:          [A: %.4f, n: %.4f, B: %.4f]' % (A_opt, 
+                                                                                 n_opt,  
                                                                                  B_opt))
 
-print('COS Optimal Parameters Error:    [A: %.4f, n: %.4f, phi: %.4f, B: %.4f]' % (A_opt_err, 
+print('COS Optimal Parameters Error:    [A: %.4f, n: %.4f, B: %.4f]' % (A_opt_err, 
                                                                                  n_opt_err, 
-                                                                                 phi_opt_err, 
                                                                                  B_opt_err))
 
-print('Percent Error:                   [A: %.4f%%, n: %.4f%%, phi: %.4f%%, B: %.4f%%]' % (np.abs(percent_error(A_opt, A_opt_err)), 
-                                                                                         np.abs(percent_error(n_opt, n_opt_err)), 
-                                                                                         np.abs(percent_error(phi_opt, phi_opt_err)), 
+print('Percent Error:                   [A: %.4f%%, n: %.4f%%, B: %.4f%%]' % (np.abs(percent_error(A_opt, A_opt_err)), 
+                                                                                         np.abs(percent_error(n_opt, n_opt_err)),  
                                                                                          np.abs(percent_error(B_opt, B_opt_err))))
 
 fig, axes = plt.subplots(1, 1, figsize = (6, 6), constrained_layout = True)
